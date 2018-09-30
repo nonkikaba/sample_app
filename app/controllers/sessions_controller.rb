1cf9f7ae9d855c1aa581a2ec2fb336c183fb9413
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-01 02:00:21 +0900

    edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    # @user.authenticate(params[:session][:password])
+    #=> Userオブジェクト　または false
+
+    if user && user.authenticate(params[:session][:password])
+    
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 end
