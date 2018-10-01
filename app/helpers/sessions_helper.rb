0e631998f0c95dbdde66c8e80232dadde3c4f33d
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-02 03:21:00 +0900

    edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
     #findで検索するとsession[:user_id]がnilの場合に例外を発生させる。find_byで検索すればsession[:user_id]がnilの時はnilを返す
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
   end
 end
