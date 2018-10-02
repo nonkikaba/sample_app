ae3fb874e1c24b36b1cdb8092c279959a1875394
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-03 00:25:09 +0900

    kk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  #helpersはデフォルトでテンプレートにincludeするが、コントローラーでは明示しなければならない
   def hello
     render html: "hello, world"
   end
