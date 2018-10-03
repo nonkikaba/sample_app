61ef5dcf3818618ed7164ffd682fff087a7e7a29
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-04 00:17:00 +0900

    1004

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember
+  attr_accessor :remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -30,5 +30,12 @@ has_secure_passwordの説明
   #ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  #永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest, User.digest(remember_token))
+    #update_attributeを使うとvalidationをスキップできる
   end
 end
