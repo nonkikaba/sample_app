a5788550f405b48e044694bf16344004399cc64d
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-04 01:01:12 +0900

    1004

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      remember user #SessionsHelper
       redirect_to user
     else
       # flash[:danger] = "Invalid email/password combination"
