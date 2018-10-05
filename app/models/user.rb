9e2cea9f88e51ecd1f46195a89ef286d736c852e
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-05 11:59:56 +0900

    1005

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,7 @@ has_secure_passwordの説明
 
   #渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
