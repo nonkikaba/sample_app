da632a9d9c479fd7d11d27abe266636a75c87d40
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-07 15:16:46 +0900

    1007

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,8 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
+    # logged_in_userメソッドでログイン状態を確認しているので、correct_userメソッドでは確認する必要がない.
+    # current_userのnilチェックは必要ない
 end
