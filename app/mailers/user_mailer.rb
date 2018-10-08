a4c4c5c828325c440ce0b3f152c85d5d4b312f64
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-09 01:03:27 +0900

    1009

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,15 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
-  def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+    # mailメソッドを実行すると以下のテンプレートが呼び出される
+    # =>  app/views/user_mailer/account_activation.text.erb
+    # =>  app/views/user_mailer/account_activation.html.erb
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset
     @greeting = "Hi"
-
     mail to: "to@example.org"
   end
 end
