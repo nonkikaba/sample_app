32d930ab91ae39bb6b37624dc74e677c9518acf9
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-20 13:47:03 +0900

    1020

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -13,6 +12,16 @@ class MicropostTest < ActiveSupport::TestCase
 
   test "user id should be present" do
     @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
 end
