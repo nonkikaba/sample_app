ed44244fa7bcd2dc5c66af4593ee8cdb1a3515d9
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-22 15:24:41 +0900

    13章

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,16 +8,28 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+    # request.referrerとする事で、元の場所（リンク元）に戻す。
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      # ログインしているユーザが投稿したmicropostsの中に、該当するidがあるかどうかを判断。
+      # ここでのidは DELETE /microposts/:id のid
+      redirect_to root_url if @micropost.nil?
     end
 end
