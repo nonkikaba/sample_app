ac6fe4a29a46195589464aca8971c86e282f99b5
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-25 15:48:39 +0900

    14章

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,14 +3,22 @@ class RelationshipsController < ApplicationController
   
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+      # app/views/relationships/create.js.erbが実行される
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+      # app/views/relationships/destroy.js.erbが実行される
+    end
   end
 end
